2ff5ea4c67690324cda39a70f486b62a28acc37a
swapnilgajbhiye21 <swapnil.gajbhiye21@gmail.com>
2023-03-17 14:25:26 +0530

    Added ArrayListDemo file

---
+++ CollectionDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list = Arrays.asList("Nayan","Pavan","Rahul","Sagar");
+		System.out.println(list);
+
+	}
+
+}
